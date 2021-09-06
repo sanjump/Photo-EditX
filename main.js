363f668dd030f30c9a40e2f8cb5d354403583edb
Sanju <mpsanju123@gmail.com>
2021-09-06 19:33:58 +0530

    1 - Added tree inside sidebar for showing files and folders 2 - Get all files and subdirectories given a directory : main.js

--- main.js
+++ main.js
@@ -1,95 +1,127 @@
-const {app, BrowserWindow, Menu, dialog,ipcMain} = require('electron')
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require('electron')
+const url = require("url");
+const path = require("path");
+const fs = require('fs');
+const glob = require('glob');
 
-ipcMain.on("msg",(event,data)=>{
-  console.log(data)
-  event.reply("r","dasdasd")
+let files = [];
+directory = 'C:\\Users\\mpsan\\OneDrive\\Desktop\\Career\\H&R block'
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
 })
 
 
-    const url = require("url");
-    const path = require("path");
+let mainWindow
 
-    let mainWindow
-
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true,
-          plugins: true
-        }
-      })
-
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/dist/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
-
-      const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
-      Menu.setApplicationMenu(mainMenu);
-
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      plugins: true
     }
+  })
 
-    app.on('ready', createWindow)
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 
-    let types = [
-     {name: 'PDF', extensions: ['pdf']}
-    ]
+  mainWindow.webContents.openDevTools()
 
-    const mainMenuTemplate = [
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  Menu.setApplicationMenu(mainMenu);
+
+  var getDirectories = function (src, callback) {
+    glob(src + '/**/*', callback);
+  };
+  getDirectories(directory, function (err, res) {
+    if (err) {
+      console.log('Error', err);
+    }
+    else {
+      console.log(res);
+    }
+  });
+
+
+}
+
+app.on('ready', createWindow)
+
+let types = [
+  { name: 'PDF', extensions: ['pdf'] }
+]
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
       {
-        label:'File',
-        submenu:[
-          {
-            label:'Open file',
-            click (mainWindow) {
-              dialog.showOpenDialog({ filters:types, properties:['openFile', 'multiSelections'] })
-              .then(function(fileObj) {
-                
-                if (!fileObj.canceled) {
-                  console.log(fileObj)
-                  // mainWindow.webContents.send('printfile', fileObj.filePaths[0].toString())
-                  
-                }
-                
-             }).catch(function(err) {
-              console.error(err)  
-           })
+        label: 'Open file',
+        click(mainWindow) {
+          dialog.showOpenDialog({ filters: types, properties: ['openFile', 'multiSelections'] })
+            .then(function (fileObj) {
+
+              if (!fileObj.canceled) {
+                console.log(fileObj)
+                // mainWindow.webContents.send('printfile', fileObj.filePaths[0].toString())
+
               }
-            }
-          ,
-          {
-            label:'Open folder',
-            click () {
-              dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] })
-              }
-          },
-          {
-            label:'Quit',
-            click(){
-              app.quit();
-            }
-          }
-        ]
+
+            }).catch(function (err) {
+              console.error(err)
+            })
+        }
       }
-    ];
+      ,
+      {
+        label: 'Open folder',
+        click() {
+          dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] })
+            .then(function (fileObj) {
+
+              if (!fileObj.canceled) {
+                console.log(fileObj)
+                // mainWindow.webContents.send('printfile', fileObj.filePaths[0].toString())
+
+              }
+
+            }).catch(function (err) {
+              console.error(err)
+            })
+        }
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
 
 
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
-    })
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-    })
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SidebarModule} from 'primeng/sidebar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-import {TabViewModule} from 'primeng/tabview';
+import { TabViewModule } from 'primeng/tabview';
+import {TreeModule} from 'primeng/tree';
+import { HttpClientModule } from '@angular/common/http';
+import { NodeService } from './node.service';
 
 @NgModule({
   declarations: [
@@ -20,9 +23,11 @@ import {TabViewModule} from 'primeng/tabview';
     ReactiveFormsModule,
     SidebarModule,
     ButtonModule,
-    TabViewModule
+    TabViewModule,
+    TreeModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NodeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
